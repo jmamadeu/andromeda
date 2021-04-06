fccdf3f33909afca895f6e76136eb2b8843ba3e7
João Amadeu <jmamadeu2000@gmail.com>
2021-04-06 20:29:29 +0100

    :zap: chore: Add tailwindcss configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,22 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          primary: '#dde3e6',
+          bold: '#1d2b37',
+          semi: '#5b646b',
+          secondary: '#81878e',
+        },
+        pink: {
+          main: '#b615b0',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
